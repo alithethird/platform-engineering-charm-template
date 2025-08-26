5472fbec3cb535cbdf8c18073a81a752898eb57d
Ali Ugur <ali.ugur@canonical.com>
2025-08-26 15:52:59 +0300

    Chore: Remove template charmcraft

--- charm/tests/integration/conftest.py
+++ charm/tests/integration/conftest.py
@@ -5,13 +5,10 @@
 
 import logging
 import os.path
-import typing
 from secrets import token_hex
 
-import boto3
 import pytest
 import pytest_asyncio
-from botocore.config import Config as BotoConfig
 from juju.application import Application
 from juju.model import Model
 from pytest import Config
@@ -346,48 +343,48 @@ async def netbox_nginx_integration_fixture(
 #     yield address
 
 
-@pytest.fixture(scope="function", name="boto_s3_client")
-def boto_s3_client_fixture(s3_netbox_configuration: dict, s3_netbox_credentials: dict):
-    """Return a S3 boto3 client ready to use
+# @pytest.fixture(scope="function", name="boto_s3_client")
+# def boto_s3_client_fixture(s3_netbox_configuration: dict, s3_netbox_credentials: dict):
+#     """Return a S3 boto3 client ready to use
 
-    Returns:
-        The boto S3 client
-    """
-    s3_client_config = BotoConfig(
-        region_name=s3_netbox_configuration["region"],
-        s3={
-            "addressing_style": "virtual",
-        },
-        # no_proxy env variable is not read by boto3, so
-        # this is needed for the tests to avoid hitting the proxy.
-        proxies={},
-    )
+#     Returns:
+#         The boto S3 client
+#     """
+#     s3_client_config = BotoConfig(
+#         region_name=s3_netbox_configuration["region"],
+#         s3={
+#             "addressing_style": "virtual",
+#         },
+#         # no_proxy env variable is not read by boto3, so
+#         # this is needed for the tests to avoid hitting the proxy.
+#         proxies={},
+#     )
 
-    s3_client = boto3.client(
-        "s3",
-        s3_netbox_configuration["region"],
-        aws_access_key_id=s3_netbox_credentials["access-key"],
-        aws_secret_access_key=s3_netbox_credentials["secret-key"],
-        endpoint_url=s3_netbox_configuration["endpoint"],
-        use_ssl=False,
-        config=s3_client_config,
-    )
-    yield s3_client
+#     s3_client = boto3.client(
+#         "s3",
+#         s3_netbox_configuration["region"],
+#         aws_access_key_id=s3_netbox_credentials["access-key"],
+#         aws_secret_access_key=s3_netbox_credentials["secret-key"],
+#         endpoint_url=s3_netbox_configuration["endpoint"],
+#         use_ssl=False,
+#         config=s3_client_config,
+#     )
+#     yield s3_client
 
 
-@pytest.fixture(scope="function", name="s3_netbox_bucket")
-def s3_netbox_bucket_fixture(
-    s3_netbox_configuration: dict, s3_netbox_credentials: dict, boto_s3_client: typing.Any
-):
-    """Creates a bucket using S3 configuration."""
-    bucket_name = s3_netbox_configuration["bucket"]
-    boto_s3_client.create_bucket(Bucket=bucket_name)
-    yield
-    objectsresponse = boto_s3_client.list_objects(Bucket=bucket_name)
-    if "Contents" in objectsresponse:
-        for c in objectsresponse["Contents"]:
-            boto_s3_client.delete_object(Bucket=bucket_name, Key=c["Key"])
-    boto_s3_client.delete_bucket(Bucket=bucket_name)
+# @pytest.fixture(scope="function", name="s3_netbox_bucket")
+# def s3_netbox_bucket_fixture(
+#     s3_netbox_configuration: dict, s3_netbox_credentials: dict, boto_s3_client: typing.Any
+# ):
+#     """Creates a bucket using S3 configuration."""
+#     bucket_name = s3_netbox_configuration["bucket"]
+#     boto_s3_client.create_bucket(Bucket=bucket_name)
+#     yield
+#     objectsresponse = boto_s3_client.list_objects(Bucket=bucket_name)
+#     if "Contents" in objectsresponse:
+#         for c in objectsresponse["Contents"]:
+#             boto_s3_client.delete_object(Bucket=bucket_name, Key=c["Key"])
+#     boto_s3_client.delete_bucket(Bucket=bucket_name)
 
 
 from collections.abc import Generator
